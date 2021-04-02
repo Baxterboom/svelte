826b84fc5e808b136334f2f03d464fe306c03e36
baxterboom <baxterboom.1@gmail.com>
2021-04-02 16:57:12 +0200

    error capture

--- src/compiler/compile/render_dom/index.ts
+++ src/compiler/compile/render_dom/index.ts
@@ -445,9 +445,9 @@ export default function dom(
 				${/* before reactive declarations */ props_inject}
 
 				${reactive_declarations.length > 0 && b`
-				$$self.$$.update = () => {
-					${reactive_declarations}
-				};
+					$$self.$$.update = () => {
+						${block.add_error_handler(reactive_declarations)}
+					};
 				`}
 
 				${fixed_reactive_declarations}
